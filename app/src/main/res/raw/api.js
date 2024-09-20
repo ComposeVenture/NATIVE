f0568a68ea7645bea9b4812b6ab0f7af7b6774b5
ComposeVenture <composeventures@gmail.com>
2024-09-20 07:50:19 +0300

    Update api.js

--- app/src/main/res/raw/api.js
+++ app/src/main/res/raw/api.js
@@ -1,4 +1,4 @@
-const FUNCTIONSAPI='https://composeventure.github.io/SERVER/Project/Movie%20Lander/MovieLander.js';
-const STYLESAPI='https://composeventure.github.io/SERVER/Project/Movie%20Lander/MovieLander.css';
+const FUNCTIONSAPI='https://composeventure.github.io/SERVER/Project/MovieLander/MovieLander.js';
+const STYLESAPI='https://composeventure.github.io/SERVER/Project/MovieLander/MovieLander.css';
 const APPMOTTO='Your Home Cinema';
 const APPNAMED='Movie Lander';
